00edbb44ae4b9395bee96fe6b51a25ac18d9e0a0
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-02-29 06:47:37 +0800

    Working on implementing the 2nd and 3rd order difference

--- do_smoothing.py
+++ do_smoothing.py
@@ -9,24 +9,46 @@ class GeneralisedStickel:
     pass
 
 
-def rotate_phases(ao, theta_rad, chan=None):
+def rotate_phases(ao1, theta_rad, chan=None):
 
     if np.isscalar(theta_rad) and chan is not None:
-        ao[:,:,chan,:] *= np.exp(1j*theta_rad)
-        return
+        ao1[:,:,chan,:] *= np.exp(1j*theta_rad)
+        return ao1
 
     if np.isscalar(theta_rad) and chan is None:
-        ao *= np.exp(1j*theta_rad)
-        return
+        ao1 *= np.exp(1j*theta_rad)
+        return ao1
 
     if not np.isscalar(theta_rad):
         # Attempt broadcasting
-        ao *= np.exp(1j*theta_rad[np.newaxis, np.newaxis, :, np.newaxis])
-        return
+        ao1 *= np.exp(1j*theta_rad[np.newaxis, np.newaxis, :, np.newaxis])
+        return ao1
 
-def optimal_rotation(ao1, ao2):
-    mean = np.nanmean(ao1 * np.conj(ao2))
+def optimal_rotation(ao1, ao2, axis=None):
+    mean = np.nanmean(ao1 * np.conj(ao2), axis=axis)
     return np.angle(mean)
+
+def ao_min_diff(ao1, ao2, idx=None):
+    '''
+    Returns the minimum difference of two (sets of) calibration solutions.
+    "Minimum" up to rotation of the solutions by a unit length phasor:
+    ao1 - e^(iθ) * ao2
+    '''
+    theta_min = optimal_rotation(ao1, ao2)
+    diff = ao1 - rotate_phases(ao2, theta_rad)
+
+    if idx is not None:
+        diff /= np.diff(idx)
+
+    return diff
+
+def ao_min_diff2(ao1, ao2, idx=None):
+
+    diff1 = ao_min_diff(ao1, ao2, idx)
+    if idx is not None:
+        diff1_idx += idx[:-1] + np.diff(idx)/2
+
+    # UP TO HERE
 
 def test_optimal_rotation(ao):
 
@@ -51,7 +73,7 @@ def test_optimal_rotation(ao):
     theta_mins = np.cumsum(delta_theta_mins)
 
     # Rotate using the found optimal thetas
-    rotate_phases(ao, theta_mins)
+    ao = rotate_phases(ao, theta_mins)
     aocal_plot.plot(ao, plot_filename="test", n_rows=6, ants_per_line=6, chan_idxs=chans)
 
 def objective(ao, ao_hat):
